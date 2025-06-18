7aae4f7b87f8fa0650d1e66abd0b24a99d5e1d0c
Arturo Sutter <62807928+aesutter@users.noreply.github.com>
2025-06-18 09:18:54 -0400

    Update members.js
    Add real phone for Arturo Sutter

--- assets/js/members.js
+++ assets/js/members.js
@@ -7,7 +7,7 @@
 // =============================================
 const csvData = `
 Name;Last Name;Phone;Email;Business Name;Website;Type of Business;Picture Filename;Show Phone?;Show Email?
-Arturo;Sutter;(+1) 305-555-1234;asutter@suttertek.com;suttertek LLC;https://suttertek.com;Technology;Art-pic001.png;Yes;Yes
+Arturo;Sutter;(+1) 305-586-7321;asutter@suttertek.com;suttertek LLC;https://suttertek.com;Technology;Art-pic001.png;Yes;Yes
 Dorothy;Griggs;(+1) 305-789-4567;dlgriggs332@gmail.com;Keyes Co.;;Real Estate;;Yes;Yes
 Sergio;Martins;(+1) 786-432-0001;Info@Sergiorealtor.com;Premier Luxe Group LLC;https://www.premierluxegroup.com;Real Estate;Sergio.jpg;Yes;Yes
 `.trim();
